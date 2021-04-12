d58d80264d10c3a10160b7d3f5bdabd27ba301fa
Ruddy Jimenez <21699756+ruddyjimenez@users.noreply.github.com>
2021-04-11 19:17:18 -0500

    Create array-loops.js

---
+++ array-loops.js
@@ -0,0 +1,5 @@
+const vegetables = ['alfalfa', 'beet', 'cabbage', 'bell pepper', 'broccoli', 'Brussels sprouts', 'carrot', 'cauliflower', 'celery', 'cucumber', 'green bean', 'lettuce', 'onion', 'pea', 'potato', 'radish', 'sweet potato', 'tomato', 'mushroom']
+
+for (let i= 0; i < vegetables.length; i++ ) {
+    console.log(vegetables[i])
+}
