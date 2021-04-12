255567711b18eb95d73213fe5c0280d30e3e051b
Ruddy <ruddy.jm@usa.com>
2021-04-12 02:46:05 -0500

    practice

---
+++ iteration.js
@@ -0,0 +1,9 @@
+const people = 56
+
+for (let i = 0; i < 56; i++){
+    if (i % 2 === 0) {
+        console.log("Person " + i + " is on the blue team")
+    } else {
+        console.log("Person " + i + " is on the red team")
+    }
+    }
