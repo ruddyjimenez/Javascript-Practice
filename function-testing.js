220e3701fd488c6aa3102177fee56c52a8599b0e
Ruddy <ruddy.jm@usa.com>
2021-04-15 13:31:35 -0500

    function practice

---
+++ function-testing.js
@@ -0,0 +1,10 @@
+function sendText(first, last, testScore) {
+    // console.log( first + ' ' + last, ', your' + 'test score' + ' ' + 'is ' + testScore)
+    console.log(`${first}, ${last} your test score is ${testScore}`)
+}
+
+sendText('Xian', 'Lee', 49)
+sendText('Anna', 'Seeberg', 89)
+sendText('Robert', 'Anderson', 78)
+sendText('Diane', 'Leming', 42)
+sendText('Mariam', 'Dover', 50)
